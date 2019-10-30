9b31bb2207f62799c6d8295c904a80589b33a345
Allan Jamil <allanjami_91@hotmail.com>
2019-10-30 13:44:09 +0100

    Added isSolved

--- src/se/nackademin/FifteenPuzzleGame.java
+++ src/se/nackademin/FifteenPuzzleGame.java
@@ -159,6 +159,35 @@ public class FifteenPuzzleGame extends JFrame implements ActionListener {
         repaint();
     }
 
+    public boolean isSolved() {
+
+        int[] winPattern = {1, 2, 3, 4,
+                5, 6, 7, 8,
+                9, 10, 11, 12,
+                13, 14, 15, 0};
+        int[] winPatternAlt = {0, 1, 2, 3, 4,
+                5, 6, 7, 8,
+                9, 10, 11, 12,
+                13, 14, 15};
+
+        int counter = 0;
+        int i = 0;
+        for (int row = 0; row < tiles.length; row++) {
+            for (int col = 0; col < tiles.length; col++) {
+                if (tiles[row][col].getIndexValue() == winPattern[i] ||tiles[row][col].getIndexValue() == winPatternAlt[i]) {
+                    counter++;
+                    i++;
+                }
+            }
+        }
+
+        if (counter == 16) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
