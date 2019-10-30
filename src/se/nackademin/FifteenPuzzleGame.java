651c8b913dd5c8bf6d0b9d274d223923ae257c6c
Pedram Shirforoushan <Pedram.Shirforoushan@yh.nackademin.se>
2019-10-30 13:16:41 +0100

    Created isSwappable method

--- src/se/nackademin/FifteenPuzzleGame.java
+++ src/se/nackademin/FifteenPuzzleGame.java
@@ -15,12 +15,19 @@ public class FifteenPuzzleGame extends JFrame implements ActionListener {
     private Tile[][] tiles = new Tile[4][4];
     private Tile tile0 = new Tile("");
 
+    private int emptyIndex;
+    private int sourceIndex;
+    private int sourceRow;
+    private int sourceCol;
+    private int blankRow;
+    private int blankCol;
+
     public FifteenPuzzleGame() {
 
         setLayout(new BorderLayout());
         add(grid, BorderLayout.CENTER);
 
-        grid.setBackground(new Color(34,37,101));
+        grid.setBackground(new Color(34, 37, 101));
         grid.setLayout(new GridLayout(4, 4));
         grid.setBorder(new EmptyBorder(50, 50, 50, 50));
         grid.setCursor(new Cursor(Cursor.HAND_CURSOR));
@@ -47,7 +54,7 @@ public class FifteenPuzzleGame extends JFrame implements ActionListener {
                     tiles[row][col].setIndexValue(i);
                     grid.add(tiles[row][col]);
                     tiles[row][col].addActionListener(this);
-                    tiles[row][col].setBackground(new Color(235,204,37));
+                    tiles[row][col].setBackground(new Color(235, 204, 37));
                     tiles[row][col].setName("tile" + i);
                     tiles[row][col].setFont(new Font("Street Cred", Font.PLAIN, 40));
                     i++;
@@ -70,8 +77,48 @@ public class FifteenPuzzleGame extends JFrame implements ActionListener {
 
     }
 
+    public boolean isSwappable(JButton button) {
+
+        // för att hitta platsen på knappen man trycker och även den blanka platsen
+        for (int row = 0; row < tiles.length; row++) {
+            for (int col = 0; col < tiles.length; col++) {
+                if (tiles[row][col] == button) {
+                    sourceRow = row;
+                    sourceCol = col;
+                } else if (tiles[row][col] == tile0) {
+                    blankRow = row;
+                    blankCol = col;
+                }
+            }
+        }
+        sourceIndex = (sourceRow * 4) + sourceCol;
+        emptyIndex = (blankRow * 4) + blankCol;
+        // om den är till höger
+        if (sourceCol != 3 && sourceRow == blankRow && tiles[sourceRow][sourceCol + 1] == tile0) {
+            return true;
+        }
+        // om den är till vänster
+        else if (sourceCol != 0 && sourceRow == blankRow && tiles[blankRow][sourceCol - 1] == tile0) {
+            return true;
+        }
+        //om den är nedanför
+        else if (sourceRow != 0 && sourceCol == blankCol && tiles[sourceRow - 1][sourceCol] == tile0) {
+            return true;
+        }
+        //om den är ovanför
+        else if (sourceRow != 3 && sourceCol == blankCol && tiles[sourceRow + 1][sourceCol] == tile0) {
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        JButton source = (JButton) e.getSource();
+
+        if (isSwappable(source)) {
+
+        }
     }
 }
